b7ba436a4b92f3cee1c68dbed07c1255317cfb85
Marioo-Ayman <mario.20375508@compit.aun.edu.eg>
2023-04-24 15:00:42 -0700

    update 3

--- write_handlers.c
+++ write_handlers.c
@@ -6,7 +6,9 @@
  * @buffer: Buffer array to handle print
  * Return: Number of chars printed.
  */
-int handle_write_char(char c , char buffer[])
+int handle_write_char(char c, char buffer[])
 {
+	c = NULL;
+
 	return (write(1, &buffer[0], 1));
 }
